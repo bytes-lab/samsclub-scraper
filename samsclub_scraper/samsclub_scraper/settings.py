74b8a2bf9e674a0f314441f04db8eaface81767f
Thomas Andrew <jason.5001001@gmail.com>
2017-04-06 16:34:09 +1000

    revise category settings

--- samsclub_scraper/samsclub_scraper/settings.py
+++ samsclub_scraper/samsclub_scraper/settings.py
@@ -22,8 +22,8 @@ NEWSPIDER_MODULE = 'samsclub_scraper.spiders'
 # ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
-
+CONCURRENT_REQUESTS = 4
+DOWNLOAD_TIMEOUT = 600
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
